c537dd2b323674079840982de6cc8abcd6297496
Mateus Nogueira <50894389+nogran@users.noreply.github.com>
2022-05-04 23:49:04 -0300

    Update Repository

--- src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
+++ src/main/java/com/generation/lojagames/repository/ProdutoRepository.java
@@ -4,12 +4,15 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.generation.lojagames.model.Produto;
 
+@Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Long>{ // Ask
 	
-	public List<Produto> findAllByNomeContainingIgnoreCase(String nome);
+	public List<Produto>findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
 	 
 	public List <Produto> findByPrecoGreaterThanOrderByPreco(BigDecimal preco);
 	 
